ba110987f73c7ccd44bc7d16dab4df62e3712790
sedi <salehisediqe86@gmail.com>
2023-07-30 07:47:21 -0700

    Shuffle Cards

--- index.js
+++ index.js
@@ -17,14 +17,23 @@
 
            const cardBackImgPath = '/images/card-back-blue.png'
 
-           const cardContainerElem = document.querySelector('.card-container')
+          
 
            let cards = []
 
            const playGameButtonElem = document.getElementById('playGame')
 
+            const cardContainerElem = document.querySelector('.card-container')
+
            const colllapsedGridAreaTemplate = ' "a a" "a a"'
            const cardCollectCellClass = ".card-pos-a"
+
+           const numCards = cardObjectDefinition.length
+
+
+           let cardPositions = []
+
+
 
           
 
@@ -98,6 +107,38 @@
                     }, index * 100)
                 })
             }
+
+            function shuffleCards(){
+                const id = setInterval(shuffle, 12)
+                let shuffleCount = 0
+                
+                function shuffle()
+                {
+
+                    randomizeCardPositions()
+
+                    if(shuffleCount == 500)
+                    {
+                        clearInterval(id)
+                    }
+                    else{
+                        shuffleCount++;
+                    }
+                }
+            }
+
+            function randomizeCardPositions(){
+
+                const random1 = Math.floor(Math.random() * numCards) + 1
+                const random2 = Math.floor(Math.random() * numCards) + 1
+                
+
+                const temp = cardPositions[random1 - 1]
+                
+                cardPositions[random1 - 1] = cardPositions[random2 - 1]
+                cardPositions[random2 - 1] = temp
+
+            }
           
 
            function createCards()
@@ -168,8 +209,14 @@
             //add card element as child element to appropriate grid cell
             addCardToGridCell(cardElem)
 
+            initializeCardPositions(cardElem)
 
 
+
+           }
+
+           function initializeCardPositions(card){
+            cardPositions.push(card.id)
            }
 
 
